9effb93b3b2f433a1538fdaed2cd51ebf13edab1
Rahib <rahibgithub@gmail.com>
2026-03-09 15:25:20 +0300

    problem 2 course 5

---
+++ Dart-pro/course5.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+enum checkprime { prime, notprime }
+
+int input(String Message) {
+  int num = 0;
+
+  do {
+    print(Message);
+
+    num = int.parse(stdin.readLineSync()!);
+  } while (num <= 0);
+  return num;
+}
+
+checkprime isprime(int num) {
+  int M = (num ~/ 2);
+  for (int i = 2; i <= M; i++) {
+    if (num % i == 0) {
+      return checkprime.prime;
+    }
+  }
+  return checkprime.notprime;
+}
+
+void printResult(int num) {
+  for (int i = 1; i <= num; i++) {
+    if (isprime(i) == checkprime.notprime) {
+      print(i);
+    }
+  }
+}
+
+void main() {
+  printResult(input("Enter Positive Number: "));
+}
